8e9dc016ba1ba17923806344aa17be6fa06edc94
zhuanghaixin <649308293@qq.com>
2018-04-19 20:37:52 +0800

    第二个轮播代码优化

--- js/index3.js
+++ js/index3.js
@@ -150,21 +150,47 @@
 
 /*第四次改进********************
  */
-$('.images>img:nth-child(1)').addClass('current');
-$('.images>img:nth-child(2)').addClass('enter');
-$('.images>img:nth-child(3)').addClass('enter');
-let n=1;
+// $('.images>img:nth-child(1)').addClass('current');
+// $('.images>img:nth-child(2)').addClass('enter');
+// $('.images>img:nth-child(3)').addClass('enter');
+// let n=1;
+//
+// setInterval(()=>{
+//     $(`.images>img:nth-child(${x(n)})`).removeClass('current').addClass('leave').one('transitionend',(e)=>{
+//         $(e.currentTarget).removeClass('leave ').addClass('enter');
+//
+//     });
+//
+//     $(`.images>img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current');
+//     n=n+1;
+// },3000);
+//
+// function x(n){
+//     if(n>3){
+//         n=n%3;
+//         if(n===0){
+//             n=3;
+//         }
+//     }
+//     return n;
+// }
 
+/*********代码优化**************/
+init();
 setInterval(()=>{
-    $(`.images>img:nth-child(${x(n)})`).removeClass('current').addClass('leave').one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave ').addClass('enter');
+    makeLeave(getImage(n)).one('transitionend',(e)=>{
+        makeEnter($(e.currentTarget));
 
     });
 
-    $(`.images>img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current');
+    makeCurrent(getImage(n+1));
     n=n+1;
 },3000);
 
+
+
+
+/*******************************/
 function x(n){
     if(n>3){
         n=n%3;
@@ -173,5 +199,22 @@ function x(n){
         }
     }
     return n;
+}
+
+function init(){
+    $('.images>img:nth-child(1)').addClass('current').siblings().addClass('enter');
+    n=1;
+}
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    return $node.removeClass('current enter').addClass('leave')
+}
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter');
+}
+function getImage(n){
+    return $(`.images>img:nth-child(${x(n)})`)
 }
 
